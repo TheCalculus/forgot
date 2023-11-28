d99b8ce19864a885de2d8e4c7175c3406cd5da91
schloing <user.datagram.protocol@proton.me>
2023-11-28 20:09:43 +1100

    isEqualTo

--- main.go
+++ main.go
@@ -1,82 +1,124 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "sync"
+	"fmt"
+	"sync"
+	"time"
 )
 
 type Entry struct {
-    data       UData
-    creation   time.Time
-    updated    time.Time
-    member_off []byte
+	data     UData
+	mapping  int
+	creation time.Time
+	updated  time.Time
+}
+
+func BasicEntry(data UData) *Entry {
+	return &Entry{data, 0, time.Now(), time.Now()}
 }
 
 type UData interface {
-    getMember(s string) interface{}
+	getMember(s string) interface{}
+	isEqualTo(d UData) bool
 }
 
 type Table struct {
-    mux      sync.Mutex
-    inmem    map[int64]*Entry
-    active   int64
+	mux    sync.Mutex
+	inmem  map[int]*Entry
+	active int
 }
 
 func (t *Table) Add(entry *Entry) {
-    t.mux.Lock()
-    defer t.mux.Unlock()
-    t.inmem[t.active] = entry
-    t.active++
+	t.mux.Lock()
+	defer t.mux.Unlock()
+
+	t.inmem[t.active] = entry
+	entry.mapping = t.active
+
+	t.active++
+}
+
+func (t *Table) Remove(entry *Entry, mult bool) {
+	t.mux.Lock()
+	defer t.mux.Unlock()
+
+	if !mult {
+		delete(t.inmem, entry.mapping)
+		return
+	}
+
+	for k, v := range t.inmem {
+		if v.data.isEqualTo(entry.data) {
+			delete(t.inmem, k)
+		}
+	}
 }
 
 func (t *Table) GetWhere(fieldName string, value interface{}) ([]*Entry, int) {
-    res := make([]*Entry, 0);
-    amt := 0
+	t.mux.Lock()
+	defer t.mux.Unlock()
 
-    for _, v := range t.inmem {
-        field := v.data.getMember(fieldName)
+	res := make([]*Entry, 0)
+	amt := 0
 
-        if field == nil {
-            return nil, -1
-        }
+	for _, v := range t.inmem {
+		field := v.data.getMember(fieldName)
 
-        if field == value {
-            res = append(res, v)
-            amt++
-        }
-    }
+		if field == nil {
+			return nil, -1
+		}
 
-    return res, amt
+		if field == value {
+			res = append(res, v)
+			amt++
+		}
+	}
+
+	return res, amt
 }
 
 // user-defined data here
 type Employee struct {
-    Name     string
-    Age      int
+	Name string
+	Age  int
 }
 
 func (e Employee) getMember(s string) interface{} {
-    switch s {
-    case "Name":
-        return e.Name;
-    case "Age":
-        return e.Age;
-    }
+	switch s {
+	case "Name":
+		return e.Name
+	case "Age":
+		return e.Age
+	}
 
-    return nil;
+	return nil
 }
 
+func (e Employee) isEqualTo(d UData) bool {
+	if other, ok := d.(Employee); ok {
+		return e.Name == other.Name && e.Age == other.Age
+	}
+	return false
+}
+
+func newEmployee(name string, age int) Employee {
+	return Employee{name, age}
+}
 
 func main() {
-    table := Table{ inmem: make(map[int64]*Entry) }
+	table := Table{inmem: make(map[int]*Entry)}
 
-    table.Add(&(Entry{ Employee { "albert einstein", 144 }, time.Now(), time.Now(), make([]byte, 0)}))
-    table.Add(&(Entry{ Employee { "albert einstein", 10 },  time.Now(), time.Now(), make([]byte, 0)}))
+	albert := BasicEntry(newEmployee("albert einstein", 144))
 
-    query, amt := table.GetWhere("Name", "albert einstein")
+	table.Add(albert)
+	table.Add(BasicEntry(newEmployee("albert einstein", 144)))
+	table.Add(BasicEntry(newEmployee("albert einstein", 10)))
 
-    for _, v := range query {
-        fmt.Println(*v)
-    }
+	table.Remove(albert, true)
+
+	query, _ := table.GetWhere("Name", "albert einstein")
+
+	for _, v := range query {
+		fmt.Println(*v)
+	}
 }
