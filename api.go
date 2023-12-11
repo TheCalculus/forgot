ebdcb2b923e98d76b4866b8de27d3aba908ca85d
thecalculus <jaisalbhasinis@gmail.com>
2023-12-11 18:30:17 +1100

    APIvalidation middleware

--- api.go
+++ api.go
@@ -1,16 +1,43 @@
 package main
 
 import (
-    "net/http"
-    "github.com/gin-gonic/gin"
+	// "net/http"
+	"github.com/gin-gonic/gin"
 )
 
+type Controller struct{}
+func (t *Controller) Default(c *gin.Context) {
+	c.JSON(200, gin.H{"response": "here it is"})
+}
+
+func APIValidation() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		secret := c.GetHeader("secret")
+
+		if secret != "ooga_booga" {
+			c.JSON(401, gin.H{"response": "bro tf up with ur api key 🤣"})
+			c.Abort()
+			
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
-    router := gin.Default()
+	router := gin.Default()
 
-    router.GET ("/getwhere/:api",     /* ... */)
-    router.POST("/add/:api/:json",    /* ... */)
-    router.GET ("/remove/:api/:json", /* ... */)
+	v1 := router.Group("/api/v1")
+	v1.Use(APIValidation())
+	{
+		controller := new(Controller)
+		v1.GET("/default", controller.Default)
+	}
 
-    router.Run("localhost:8080")
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{"response": "not found"})
+	})
+
+	router.Run("localhost:8080")
 }
