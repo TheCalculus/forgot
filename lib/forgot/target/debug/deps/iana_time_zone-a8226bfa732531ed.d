3ecf8db4888f57785e320fa971cd1bdf813835cb
schloing <user.datagram.protocol@proton.me>
2024-01-02 11:44:17 +1100

    .

---
+++ lib/forgot/target/debug/deps/iana_time_zone-a8226bfa732531ed.d
@@ -0,0 +1,9 @@
+/mnt/c/Users/jaisal/projects/forgot/lib/forgot/target/debug/deps/libiana_time_zone-a8226bfa732531ed.rmeta: /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/lib.rs /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/ffi_utils.rs /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/tz_linux.rs
+
+/mnt/c/Users/jaisal/projects/forgot/lib/forgot/target/debug/deps/libiana_time_zone-a8226bfa732531ed.rlib: /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/lib.rs /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/ffi_utils.rs /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/tz_linux.rs
+
+/mnt/c/Users/jaisal/projects/forgot/lib/forgot/target/debug/deps/iana_time_zone-a8226bfa732531ed.d: /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/lib.rs /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/ffi_utils.rs /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/tz_linux.rs
+
+/home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/lib.rs:
+/home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/ffi_utils.rs:
+/home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/tz_linux.rs:
