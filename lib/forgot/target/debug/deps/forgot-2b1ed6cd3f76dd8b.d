4e1b160a017629853fba3ece491bf80bc22645d5
thecalculus <jaisalbhasinis@gmail.com>
2024-01-02 11:44:42 +1100

    .gitignore

--- lib/forgot/target/debug/deps/forgot-2b1ed6cd3f76dd8b.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/jaisal/projects/forgot/lib/forgot/target/debug/deps/libforgot-2b1ed6cd3f76dd8b.rmeta: src/main.rs
-
-/mnt/c/Users/jaisal/projects/forgot/lib/forgot/target/debug/deps/forgot-2b1ed6cd3f76dd8b.d: src/main.rs
-
-src/main.rs:
--- lib/forgot/target/debug/deps/iana_time_zone-793681594a7cd2a0.d
+++
@@ -1,9 +0,0 @@
-/mnt/c/Users/jaisal/projects/forgot/lib/forgot/target/debug/deps/libiana_time_zone-793681594a7cd2a0.rmeta: /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/lib.rs /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/ffi_utils.rs /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/tz_linux.rs
-
-/mnt/c/Users/jaisal/projects/forgot/lib/forgot/target/debug/deps/libiana_time_zone-793681594a7cd2a0.rlib: /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/lib.rs /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/ffi_utils.rs /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/tz_linux.rs
-
-/mnt/c/Users/jaisal/projects/forgot/lib/forgot/target/debug/deps/iana_time_zone-793681594a7cd2a0.d: /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/lib.rs /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/ffi_utils.rs /home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/tz_linux.rs
-
-/home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/lib.rs:
-/home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/ffi_utils.rs:
-/home/jaisal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.58/src/tz_linux.rs:
