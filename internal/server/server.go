a90e13c88da6993bb58686d35656cb4541da9c85
schloing <user.datagram.protocol@proton.me>
2024-09-19 22:18:49 +1000

    .

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,4 +1,4 @@
-package forgot
+package server
 
 import (
 	"net/http"
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "forgot/forgot"
+    "github.com/thecalculus/forgot/internal"
 )
 
 func main() {
